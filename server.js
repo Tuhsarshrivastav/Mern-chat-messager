eb923e14f260be228c16582740a4813af2768678
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 <ts3657@gmail.com>
2021-12-07 14:08:29 +0530

    connected with database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require("express");
+const connectDB = require('./config/db');
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB()
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`app listening on port ${port}!`));
